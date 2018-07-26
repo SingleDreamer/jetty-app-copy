232f5b7c6330363b185edc2db7377119d941260e
Jessica Ng <jessica.h.ng@accenture.com>
2018-07-26 14:10:47 -0400

    Trying to fix the http post request error

--- src/main/java/org/example/app/resources/TaskResource.java
+++ src/main/java/org/example/app/resources/TaskResource.java
@@ -1,4 +1,4 @@
-package org.example.app.resources;
+ package org.example.app.resources;
 
 import org.example.app.api.Task;
 import org.skife.jdbi.v2.DBI;
@@ -6,56 +6,57 @@ import org.skife.jdbi.v2.Handle;
 
 import java.util.Iterator;
 
-import javax.ws.rs.*;
+//import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 
 @Path("/task")
-public class TaskResource { 
-	
+public class TaskResource {
+
 	DBI dbi = new DBI(MyDAO.TASK_DB_URL,
             MyDAO.TASK_DB_USER, MyDAO.TASK_DB_PASS);
 	//Handle h = dbi.open();
 	//MyDAO dao = h.attach(MyDAO.class);
 	MyDAO dao = dbi.onDemand(MyDAO.class);
-	
-	
+
+
     @GET
     @Path("/{param}")
-    @Produces(MediaType.APPLICATION_JSON)
-    //@Produces({MediaType.TEXT_PLAIN})
-    /*public String taskGet(@PathParam("param") int id) {
+    @Produces({MediaType.TEXT_PLAIN})
+    public String taskGet(@PathParam("param") int id) {
         return dao.findById(id);
-    }*/
-    public Task taskGet(@PathParam("param") int id) {
-        return new Task(id, dao.findById(id));
     }
-    
+		//@Produces(MediaType.APPLICATION_JSON)
+    /*public Task taskGet(@PathParam("param") int id) {
+        return new Task(id, dao.findById(id));
+    }*/
+
     @PUT
-    @Path("/{param}") 
+    @Path("/{param}")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.TEXT_PLAIN})
     public String taskPut(@PathParam("param") int id, Task task) {
     	dao.update(id, task.getTodo());
     	return task.getTodo();
     }
-    
+
     @DELETE
-    @Path("/{param}") 
+    @Path("/{param}")
     @Produces({MediaType.TEXT_PLAIN})
     public String taskDelete(@PathParam("param") int id) {
-    	dao.delete(id); 
-    	return "deleted " + id; 
+    	dao.delete(id);
+    	return "deleted " + id;
     }
-    
-    @GET 
+
+    @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String taskGetAll() {
-    	Iterator<Task> i = dao.getAll(); 
+    	Iterator<Task> i = dao.getAll();
     	String s = "";
-    	Task i_next; 
+    	Task i_next;
     	while (i.hasNext()) {
-    		i_next = i.next(); 
+    		i_next = i.next();
     		System.out.println(i_next.getId() + " " + i_next.getTodo());
     		s = s + i_next.getId() + " " + i_next.getTodo();
     	}
@@ -63,14 +64,21 @@ public class TaskResource {
     }
 
     @POST
+		@Consumes({MediaType.APPLICATION_JSON})
+		//@Consumes("application/json")
     @Produces(MediaType.TEXT_PLAIN)
     public String taskPostJson(Task task) {
 //    	dao.insert(Task.id_counter, task.getTodo());
+			System.out.println("POST was done");
     	dao.insert(task.getTodo());
         return "You just did a POST for the task: " + task.getTodo() + "\n";
     }
-    
-    
+	/*	public Task taskPostJson(Task task) {
+			String result = "You just did a POST for the task: " + task.getTodo() + "\n";
+			return Response.status(201).entity(result).build();
+		}*/
+
+
     //h.close();
 
 }
