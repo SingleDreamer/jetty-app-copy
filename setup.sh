65747e15d65db207c188302a5483c58a2887de73
Jessica Ng <jessica.h.ng@accenture.com>
2018-08-03 06:11:25 -0400

    edited setup.sh

--- setup.sh
+++ setup.sh
@@ -48,7 +48,14 @@ docker run --detach \
 
 python going_headless/get_jenkins_password.py
 
-GITLAB_PERSONAL_ACCESS_TOKEN="$ curl http://192.168.99.100:30080/api/v4/session --data 'login=root&password=*HnYnKkn4869' | jq '.private_token' curl http://192.168.99.100:30080/api/v4/session --data 'login=root&password=*HnYnKkn4869' | jq '.private_token'"
+GITLAB_PRIVATE_ACCESS_TOKEN="$(curl http://192.168.99.100:30080/api/v4/session --data 'login=root&password=*HnYnKkn4869' | jq '.private_token' curl http://192.168.99.100:30080/api/v4/session --data 'login=root&password=*HnYnKkn4869' | jq '.private_token')"
+echo GITLAB_PRIVATE_ACCESS_TOKEN
+GITLAB_PERSONAL_ACCESS_TOKEN="$(curl --request POST --header "PRIVATE-TOKEN: TqffaQpkuMcxiixSfADs" --data "name=mytoken" --data "expires_at=2018-08-07" --data "scopes[]=api" http://192.168.99.100:30080 /api/v4/users/1/impersonation_tokens | jq '.token')"
+echo GITLAB_PERSONAL_ACCESS_TOKEN
+
+python going_headless/add_credentials.py
+python going_headless/configure.py
+
 
 # local docker registry
 docker run -d -p 5000:5000 --restart=always --name $REGISTRY_NAME registry:2
